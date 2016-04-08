e727032415bdefba873bd78b1e738db98b6a6d3b
Somasundaram Mahesh <msomasundaram93@gmail.com>
2016-04-09 01:28:50 +0530

    changed list item ui and removed fab

--- app/src/main/java/com/msomu/sunshine/MainActivity.java
+++ app/src/main/java/com/msomu/sunshine/MainActivity.java
@@ -3,14 +3,11 @@ package com.msomu.sunshine;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,14 +26,6 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.container, new ForecastFragment(), FORECASTFRAGMENT_TAG)
                     .commit();
         }
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
