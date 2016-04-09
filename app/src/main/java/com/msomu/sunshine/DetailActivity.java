412d525a1e7a70aee96dcb79cbf93bcf3d69c44a
Somasundaram Mahesh <msomasundaram93@gmail.com>
2016-04-10 03:13:22 +0530

    styles and ripple with phone fix

--- app/src/main/java/com/msomu/sunshine/DetailActivity.java
+++ app/src/main/java/com/msomu/sunshine/DetailActivity.java
@@ -17,8 +17,14 @@ public class DetailActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         if (savedInstanceState == null) {
+            Bundle arguments = new Bundle();
+            arguments.putParcelable(DetailActivityFragment.DETAIL_URI, getIntent().getData());
+
+            DetailActivityFragment fragment = new DetailActivityFragment();
+            fragment.setArguments(arguments);
+
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.weather_detail_container, new DetailActivityFragment())
+                    .add(R.id.weather_detail_container, fragment)
                     .commit();
         }
     }
