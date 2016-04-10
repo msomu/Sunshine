e63848d2dd1aebfe1595ffb1b8932b1a2c0751ac
Somasundaram Mahesh <msomasundaram93@gmail.com>
2016-04-10 18:52:09 +0530

    Notify as synced

--- app/src/main/java/com/msomu/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/msomu/sunshine/ForecastAdapter.java
@@ -99,11 +99,11 @@ public class ForecastAdapter extends CursorAdapter {
         viewHolder.iconView.setContentDescription(description);
         boolean isMetric = Utility.isMetric(context);
         double high = cursor.getDouble(ForecastFragment.COL_WEATHER_MAX_TEMP);
-        viewHolder.highTempView.setText(Utility.formatTemperature(context, high, isMetric));
+        viewHolder.highTempView.setText(Utility.formatTemperature(context, high));
 
         // Read low temperature from cursor
         double low = cursor.getDouble(ForecastFragment.COL_WEATHER_MIN_TEMP);
-        viewHolder.lowTempView.setText(Utility.formatTemperature(context, low, isMetric));
+        viewHolder.lowTempView.setText(Utility.formatTemperature(context, low));
     }
 
     /**
