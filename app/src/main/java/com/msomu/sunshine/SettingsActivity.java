0e0ea1e77005ca17d2c7a446812cfe074e0af7b9
Somasundaram Mahesh <msomasundaram93@gmail.com>
2016-05-01 00:59:23 +0530

    bug

--- app/src/main/java/com/msomu/sunshine/SettingsActivity.java
+++ app/src/main/java/com/msomu/sunshine/SettingsActivity.java
@@ -36,6 +36,22 @@ public class SettingsActivity extends PreferenceActivity
         bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_units_key)));
     }
 
+    // Registers a shared preference change listener that gets notified when preferences change
+    @Override
+    protected void onResume() {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        sp.registerOnSharedPreferenceChangeListener(this);
+        super.onResume();
+    }
+
+    // Unregisters a shared preference change listener
+    @Override
+    protected void onPause() {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        sp.unregisterOnSharedPreferenceChangeListener(this);
+        super.onPause();
+    }
+
     /**
      * Attaches a listener so the summary is always updated with the preference value.
      * Also fires the listener once, to initialize the summary (so it shows up before the value
@@ -65,23 +81,23 @@ public class SettingsActivity extends PreferenceActivity
             int prefIndex = listPreference.findIndexOfValue(stringValue);
             if (prefIndex >= 0) {
                 preference.setSummary(listPreference.getEntries()[prefIndex]);
-            } else if (key.equals(getString(R.string.pref_location_key))) {
-                @SunshineSyncAdapter.LocationStatus int status = Utility.getLocationStatus(this);
-                switch (status) {
-                    case SunshineSyncAdapter.LOCATION_STATUS_OK:
-                        preference.setSummary(stringValue);
-                        break;
-                    case SunshineSyncAdapter.LOCATION_STATUS_UNKNOWN:
-                        preference.setSummary(getString(R.string.pref_location_unknown_description, value.toString()));
-                        break;
-                    case SunshineSyncAdapter.LOCATION_STATUS_INVALID:
-                        preference.setSummary(getString(R.string.pref_location_error_description, value.toString()));
-                        break;
-                    default:
-                        // Note --- if the server is down we still assume the value
-                        // is valid
-                        preference.setSummary(stringValue);
-                }
+            }
+        } else if (key.equals(getString(R.string.pref_location_key))) {
+            @SunshineSyncAdapter.LocationStatus int status = Utility.getLocationStatus(this);
+            switch (status) {
+                case SunshineSyncAdapter.LOCATION_STATUS_OK:
+                    preference.setSummary(stringValue);
+                    break;
+                case SunshineSyncAdapter.LOCATION_STATUS_UNKNOWN:
+                    preference.setSummary(getString(R.string.pref_location_unknown_description, value.toString()));
+                    break;
+                case SunshineSyncAdapter.LOCATION_STATUS_INVALID:
+                    preference.setSummary(getString(R.string.pref_location_error_description, value.toString()));
+                    break;
+                default:
+                    // Note --- if the server is down we still assume the value
+                    // is valid
+                    preference.setSummary(stringValue);
             }
         } else {
             // For other preferences, set the summary to the value's simple string representation.
